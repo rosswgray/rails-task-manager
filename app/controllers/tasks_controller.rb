234d6e9f50b5de4f012bf9ad55a630812bac5fdd
Ross Gray <rosswgray@gmail.com>
2020-11-17 15:09:03 +0800

    completed links and form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,25 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
   def show
     @task = Task.find(params[:id])
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
 
+    redirect_to task_path(@task)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'new', to: 'tasks#new', as: :new_task
+
+  post 'tasks', to: 'tasks#create'
+
 end
